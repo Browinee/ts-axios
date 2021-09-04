d47a1a518e1b78efedb900ff0da4e321fe4329df
justin.lin <justin.lin@yxunistar.com>
2021-09-04 10:16:47 +0800

    feat: add create function

--- src/axios.ts
+++ src/axios.ts
@@ -1,15 +1,19 @@
-import {AxiosInstance, AxiosRequestConfig} from "./types";
+import {AxiosInstance, AxiosRequestConfig, AxiosStatic} from "./types";
 import Axios from "./core/Axios";
 import {extend} from "./helpers/util";
 import defaults from "./defaults";
+import mergeConfig from "./core/mergeConfig";
 
-function getAxios (config: AxiosRequestConfig): AxiosInstance {
+function getAxios (config: AxiosRequestConfig): AxiosStatic {
     const context = new Axios(config);
     const axios = Axios.prototype.request.bind(context)
 
     extend(axios, context);
-    return axios as AxiosInstance;
+    return axios as AxiosStatic;
 }
 
 const axios = getAxios(defaults);
+axios.create = function(config: AxiosRequestConfig) {
+    return getAxios(mergeConfig(defaults, config));
+}
 export default axios;
